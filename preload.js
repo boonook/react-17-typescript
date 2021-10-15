0aace4b173284ba79c927c6c0c2da1b49609f22d
大地瓜 <boonook@163.com>
2021-10-15 15:38:26 +0800

    更新

--- preload.js
+++ preload.js
@@ -1,3 +1,14 @@
 global.electron = require('electron');
 window.ipcRenderer = require('electron').ipcRenderer;
 window.remote = require('electron').remote;
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const type of ['chrome', 'node', 'electron']) {
+      replaceText(`${type}-version`, process.versions[type])
+    }
+})
+  
